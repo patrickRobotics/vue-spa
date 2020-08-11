1f9f16d6369d5257722360c2cf2f9cc67c9d29d3
John Doe <pirungu9@gmail.com>
2020-08-11 22:58:07 +0300

    hot-reloading

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ import Vue from "vue";
 
 const app = new Vue({
   data: {
-    hello: "hey there!"
-  }
+    hello: "hey there 3!"
+  },
+  template: '<div id="app">{{ hello }}</div>'
 });
 
 export { app };
