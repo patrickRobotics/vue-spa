70b75f585026d01e5c467de3cc087291edc3ecda
John Doe <pirungu9@gmail.com>
2020-08-12 11:54:31 +0300

    vue component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 import Vue from 'vue'
 
+Vue.component('app', {
+  template: `
+    <div id="app">
+      <nav class="nav has-shadow">
+        <div class="container">
+          <a href="/">
+            <img src="http://shorturl.at/uzCNZ" alt="Vue SPA">
+          </a>
+        </div>
+      </nav> 
+      <section class="main-section section"></section>
+      <footer class="footer">
+        <div class="content has-text-centered">
+          Follow us on
+          <a href="https://twitter.com/patrickxpin" target="_blank">Twitter</a>
+        </div> 
+      </footer>
+    </div>
+  `
+})
+
 const app = new Vue({
-  data: {
-    hello: 'hey there 3!'
-  },
-  template: '<div id="app">{{ hello }}</div>'
+  render: h => h('app')
 })
 
 export { app }
